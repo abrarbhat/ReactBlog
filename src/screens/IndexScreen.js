a24ba54ec2301e111c955ac5bb8a9e767957fc13
abrarbhat <abrarbhat008@gmail.com>
2020-11-21 12:53:04 +0530

    Initial Commit With Context

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+
+const IndexScreen =()=>{
+   
+    return(
+        <View >
+            <Text>Index os is strongl; Screen</Text>
+        </View>
+    );
+};
+
+const styles= StyleSheet.create({});
+
+export default IndexScreen;
